4b04dd899132fc6165671cad8311b3febd5bcbab
serch1322 <51964536+serch1322@users.noreply.github.com>
2020-06-17 12:15:46 -0500

    Add files via upload

---
+++ factura/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Factura Suma MX',
+    'author': 'IT REINGENIERIAS S. DE R.L.',
+    'summary': 'Archivo de Impresión para Suma MX',
+    'version': '13.0.0.1',
+    'data': [
+        'report/factura_suma_print.xml',
+    ],
+    'dependencies': [
+        'account_accountant',
+    ],
+}
